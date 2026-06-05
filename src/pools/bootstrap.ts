422dffffa93edfe9bb8f65f1ec72712b02391c53
stenkaq <vdyugay2004@gmail.com>
2026-06-05 16:06:42 +0500

    feat: upd pool bootstraping

--- src/pools/bootstrap.ts
+++ src/pools/bootstrap.ts
@@ -3,12 +3,19 @@ import { AlchemyGatewayImpl } from "./infrastucture/alchemy-gateway.js";
 import { GraphStudioGatewayImpl } from "./infrastucture/graph-studio-gateway.js";
 import { PoolManagerImpl } from "./domain/pool-manager.js";
 import { PoolServiceImpl } from "./domain/pool-service.js";
+import { PoolEventHandler } from "./infrastucture/pool-event-handler.js";
 
 export async function bootstrap() {
   const graphStudio = new GraphStudioGatewayImpl(appConfig.graphStudio);
-  const alchemy = new AlchemyGatewayImpl();
+  const alchemy = new AlchemyGatewayImpl(appConfig.alchemy);
   const poolManager = new PoolManagerImpl();
 
   const poolService = new PoolServiceImpl(graphStudio, alchemy, poolManager);
   await poolService.getPools();
+
+  const eventHandler = new PoolEventHandler(
+    poolManager,
+    appConfig.alchemy.wsUrl,
+  );
+  eventHandler.start();
 }
