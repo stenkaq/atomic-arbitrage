2e38af54a64ad5dc2bf039a6d916cfa6bd11f1a4
stenkaq <vdyugay2004@gmail.com>
2026-06-05 16:06:08 +0500

    feat: PoolManager add logic to update state of pool

--- src/pools/domain/pool-manager.ts
+++ src/pools/domain/pool-manager.ts
@@ -1,31 +1,60 @@
-import { UniswapV3Pool } from "./pool";
+import { UniswapV3Pool, UniswapV3PoolState } from "./pool";
 
 export interface PoolManager {
   add(pool: UniswapV3Pool): void;
   get(address: string): UniswapV3Pool;
   has(address: string): boolean;
+  updateState(address: string, state: Partial<UniswapV3PoolState>): void;
+  updateTick(
+    address: string,
+    tickLower: number,
+    tickUpper: number,
+    liquidityDelta: bigint,
+  ): void;
+  addresses(): string[];
 }
 
 export class PoolManagerImpl implements PoolManager {
-  public poolMap: Map<string, UniswapV3Pool>;
+  private readonly poolMap = new Map<string, UniswapV3Pool>();
 
-  constructor() {
-    this.poolMap = new Map();
-  }
-
-  public add(pool: UniswapV3Pool): void {
+  add(pool: UniswapV3Pool): void {
     this.poolMap.set(pool.address, pool);
   }
 
-  public get(address: string): UniswapV3Pool {
+  get(address: string): UniswapV3Pool {
+    return this.getPoolOrThrow(address);
+  }
+
+  has(address: string): boolean {
+    return this.poolMap.has(address);
+  }
+
+  updateState(address: string, state: Partial<UniswapV3PoolState>): void {
+    this.getPoolOrThrow(address).syncState(state);
+  }
+
+  updateTick(
+    address: string,
+    tickLower: number,
+    tickUpper: number,
+    liquidityDelta: bigint,
+  ): void {
+    this.getPoolOrThrow(address).applyLiquidityDelta(
+      tickLower,
+      tickUpper,
+      liquidityDelta,
+    );
+  }
+
+  addresses(): string[] {
+    return Array.from(this.poolMap.keys());
+  }
+
+  private getPoolOrThrow(address: string): UniswapV3Pool {
     const pool = this.poolMap.get(address);
 
     if (pool) return pool;
 
     throw new Error(`[PoolManager.get] Pool not found. address: ${address}`);
-  }
-
-  public has(address: string): boolean {
-    return this.poolMap.has(address);
   }
 }
