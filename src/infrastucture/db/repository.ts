9a412cd682b2b78089e2ebd80529e8cbba4d025f
stenkaq <vdyugay2004@gmail.com>
2026-06-11 11:53:55 +0500

    feat: upd customRepository

--- src/infrastucture/db/repository.ts
+++ src/infrastucture/db/repository.ts
@@ -1,35 +1,46 @@
 import { Model } from "mongoose";
 
-export abstract class CustomRepository<T> {
+export abstract class CustomRepository<M, T> {
   constructor(protected readonly model: Model<T>) {}
 
-  async findAll(): Promise<T[]> {
-    return this.model.find() as unknown as Promise<T[]>;
+  protected abstract toDomain(doc: T): M;
+  protected abstract toSchema(entity: M): T;
+
+  async findAll(): Promise<M[]> {
+    const docs = await this.model.find();
+    return docs.map((doc) => this.toDomain(doc as unknown as T));
   }
 
-  async findById(id: string): Promise<T | null> {
-    return this.model.findById(id) as unknown as Promise<T | null>;
+  async findById(id: string): Promise<M | null> {
+    const doc = await this.model.findById(id);
+    return doc ? this.toDomain(doc as unknown as T) : null;
   }
 
   async deleteById(id: string): Promise<void> {
     await this.model.findByIdAndDelete(id);
   }
 
-  protected async upsertById(id: string, data: Partial<T>): Promise<void> {
-    await this.model.findByIdAndUpdate(
+  async save(entity: M): Promise<M> {
+    const doc = await this.model.create(this.toSchema(entity));
+    return this.toDomain(doc as unknown as T);
+  }
+
+  protected async upsertById(id: string, entity: M): Promise<M> {
+    const doc = await this.model.findByIdAndUpdate(
       id,
-      { $set: { _id: id, ...data } } as any,
+      { $set: { _id: id, ...entity } },
       { upsert: true, new: true },
     );
+    return this.toDomain(doc as unknown as T);
   }
 
   protected async upsertManyById(
-    items: Array<{ id: string; data: Partial<T> }>,
+    items: Array<{ id: string; entity: M }>,
   ): Promise<void> {
-    const ops = items.map(({ id, data }) => ({
+    const ops = items.map(({ id, entity }) => ({
       updateOne: {
         filter: { _id: id },
-        update: { $set: data },
+        update: { $set: entity },
         upsert: true,
       },
     }));
