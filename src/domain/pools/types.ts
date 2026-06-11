f2e4f6d77b3584140eb2018e7fd326cdc80a593d
stenkaq <vdyugay2004@gmail.com>
2026-06-11 14:50:53 +0500

    feat: upd Pool types

---
+++ src/domain/pools/types.ts
@@ -0,0 +1,6 @@
+export type UniswapV3PoolData = {
+  tickSpacing: number;
+  liquidity: bigint;
+  sqrtPriceX96: bigint;
+  tick: number;
+};
--- src/external/domain/pools/types.ts
+++ src/external/domain/pools/types.ts
@@ -4,7 +4,7 @@ export interface Token {
   decimals: string;
 }
 
-export interface UniswapV3Pool {
+export interface ExternalUniswapV3Pool {
   id: string;
   feeTier: string;
   token0: Token;
@@ -12,7 +12,7 @@ export interface UniswapV3Pool {
   totalValueLockedUSD: string;
 }
 
-export interface UniswapV3PoolState {
+export interface ExternalUniswapV3PoolData {
   tickSpacing: number;
   liquidity: string;
   sqrtPriceX96: string;
